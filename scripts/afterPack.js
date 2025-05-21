ff9fe4d5336f1817640bb6cfc65202e17b598c4c
lkavindu199 <lkavindu199@gmail.com>
2025-05-21 10:09:57 +0530

    fix:

--- scripts/afterPack.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = async (context) => {
-  const updateFilePath = path.join(context.appOutDir, 'Resources', 'app-update.yml');
-  if (fs.existsSync(updateFilePath)) {
-    fs.unlinkSync(updateFilePath);
-  }
-};
